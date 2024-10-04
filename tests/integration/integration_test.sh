acb9aba0f8130e45c865ff79ccbc092017fb2617
Kiera Bennett <Kiera.Bennett@ibm.com>
2024-10-04 15:48:05 +0100

    Change debugger default

--- tests/integration/integration_test.sh
+++ tests/integration/integration_test.sh
@@ -5,12 +5,12 @@ set -eux # This is important to ensure that return codes from failing tests are 
 
 function __run_test__(){
     local playbook=$1
-    local debug_me=${2:-foo}
-    local inventory_path=${ANSIBLE_COLLECTIONS_PATH}/ansible_collections/ibm/ibm_zos_cics_operator/inventories/inventory_zos.yml
+    local debug_me=${2:-0}
+    local inventory_path=${ANSIBLE_COLLECTIONS_PATH}/ansible_collections/ibm/zos_cics_operator/inventories/inventory_zos.yml
     export ANSIBLE_INVENTORY_ANY_UNPARSED_IS_FAILED=True
     export ANSIBLE_INSTALL_LOCATION=/usr/local/bin/ansible-playbook
 
-    if [ $debug_me -eq 1 ]
+    if [ "$debug_me" -eq 1 ]
     then
         python -m debugpy --wait-for-client --listen 0.0.0.0:5678 $ANSIBLE_INSTALL_LOCATION -i $inventory_path $playbook
     else
